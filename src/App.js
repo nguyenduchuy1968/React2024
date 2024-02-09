18a957969b3914083d645d5f558641e5de6e5e5b
YOUR_NAME <YOUR_EMAIL>
2024-02-09 12:49:38 +0200

    lesson-3/part_1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import {useRef, useState} from "react";
 
 
 const App = () => {
+    // const name = useRef()
+    // const age = useRef()
+
+    const [user, setUser] = useState({})
+
+    const handler = (e) => {
+        e.preventDefault()
+        // console.log(e.target.name.value);
+        // console.log(e.target.age.value)
+        setUser(prev => ({...prev, [e.target.name] : e.target.value}))
+
+    }
+
+        console.log(user)
+
+
   return (
-      <div>
-      </div>
+      <form>
+          <input type={'text'} placeholder={'username'} name={'name'} onChange={handler}/>
+          <input type={'text'} placeholder={'age'} name={'age'} onChange={handler}/>
+          <button>Save</button>
+      </form>
   );
 };
 
